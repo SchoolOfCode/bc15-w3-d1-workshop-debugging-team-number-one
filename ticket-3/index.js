aca887dcde8b2fbc1f8e49301316ffc95355fb26
Danny Ryan <dannykryan@gmail.com>
2023-09-18 16:21:18 +0100

    Ticket 3 complete

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,8 +1,10 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
 
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
+  const numberString = number.toString();
+
+  for (let i = 0; i < numberString.length; i++) {
+    const digit = numberString[i];
     if ("9" === digit) {
       count++;
     }
@@ -10,3 +12,8 @@ function countNumberOfNinesInNumber(number) {
 
   return count;
 }
+
+console.log(countNumberOfNinesInNumber(99.999));
+console.log(countNumberOfNinesInNumber(9));
+console.log(countNumberOfNinesInNumber(989));
+console.log(countNumberOfNinesInNumber(1115.1257846));
